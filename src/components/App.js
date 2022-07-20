bbe2eee0e25fccf45b87b5a217c8ebc99b4ae6ef
AyeshaShaharyar <ayesha2598@gmail.com>
2022-07-19 23:05:23 -0400

    create area to add user inputs

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 import { notes } from "../notes";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((noteItem) => (
         <Note
           key={noteItem.key}
