3f81e49df4e01b562cb0d7356f6a5a6d0b267e84
AyeshaShaharyar <ayesha2598@gmail.com>
2022-07-19 23:01:27 -0400

    add dynamic notes data

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-export default function Note() {
+
+export default function Note(props) {
+  
   return (
     <div className="note">
-      <h1>Heading</h1>
-      <p> Paragaragph</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
